025f7c9ce7cb8678ddf458980998b4003625d6ee
ealpha072 <ealpha072@gmail.com>
2023-06-14 10:30:00 +0300

    Change to hotel management system

--- food_odering_system/hotel_system.c
+++ food_odering_system/hotel_system.c
@@ -28,3 +28,40 @@ struct hotels{
 // Initialize the hotels and details
 struct hotels m[5];
 struct userDetais s[100];
+
+//inpput new account
+void signUp()
+
+int choice;
+
+int main(){
+    while(1){
+        printf("\n\nWelcome to Alpha fast foods\n");
+
+        printf("\n\n\nMENU");
+        printf("1) SIGNUP");
+        printf("2)LOGIN");
+        printf("3) EXIT");
+        printf("Enter your choice here: ");
+
+        switch (choice){
+            case 1:{
+                signup();
+                break;
+            }
+
+            case 2:{
+                login();
+                break;
+            }
+
+            case 3: {
+                exit(1);
+            }
+
+            default: {
+                printf("\nPlease Enter the valid choice\n");
+            }
+        }
+    }
+}
