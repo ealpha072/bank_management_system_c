3243a8e610b1326f3211fa328a4a0fe0e74fbe5e
ealpha072 <ealpha072@gmail.com>
2023-09-26 10:30:00 +0300

    Remove old code

--- bank_system.c
+++ bank_system.c
@@ -5,34 +5,11 @@
 #include <stdlib.h>
 
 int main_exit;
-struct date{
-    int month, day, year;
-};
-
-struct{
-    char name[60];
-    int acc_no, age;
-    char email_addess[60];
-    int id_no;
-    long long phone_no;
-    char acc_type[10];
-    float amount;
-    struct date dob;
-    struct date deposit;
-    struct date withdraw;
-} add, update, check, rem, transaction;
-
-float interest(float t, float amount, int rate){
-    float SI;
-    SI = (rate * t * amount)/100.0;
-    return (SI);
-}
-
 
 void menu(void){
     int choice;
     system("cls");
-    system("color 6");
+    system("color 7");
     printf("\n\n\t\tCUSTOMER ACCOUNT BANK MANAGEMENT SYSTEM");
     printf("\n\n\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Welcome to the main menu \xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
     printf("\n\t1.Create new account");
@@ -60,75 +37,6 @@ void menu(void){
 
 void close_app(){
     printf("\n\nThank you for choosing Alpha's Bank system");
-}
-
-void new_account(){
-    time_t t = time(NULL);
-    struct tm *current_time;
-    current_time = localtime(&t);
-    char s[11];
-    strftime(s, sizeof(s), "%m/%d/%Y", current_time);
-
-    FILE *ptr;
-
-
-    if(access("data_records.csv", F_OK) == 0){
-        //OPEN IN APPEND MODE
-        ptr = fopen("data_records.csv", "a");
-        if(ptr == NULL){
-            perror("Unable to open file");
-            exit(0);
-        }
-    }else{
-        //OPEN IT AND WRITE HEADERS
-        ptr = fopen("data_records.csv", "w");
-        if(ptr == NULL){
-            perror("Unable to open file");
-            exit(0);
-        }
-        fprintf(ptr, "Date,name,acc_no,age,email_address,id_no,phone_no,acc_type");
-        //s, add.name, add.acc_no, add.age, add.address, add.id_no, add.phone_no, add.acc_type
-    }
-
-    system("cls");
-    printf("\n\n\t\xB2\xB2\xB2 ADD RECORD \xB2\xB2\xB2\xB2");
-    //printf("\n\n\n Enter today's date ()mm/dd/yyyy: ");
-    //scanf("%d/%d/%d", &add.deposit.month, &add.deposit.day, &add.deposit.year);
-    printf("\n Enter account number: ");
-    scanf("%d", &add.acc_no);
-    printf("\n Enter your name: ");
-    scanf("%s", add.name);
-    printf("\n Enter your date of birth(mm/dd/yyy): ");
-    scanf("%d/%d/%d", &add.dob.month, &add.dob.day, &add.dob.year);
-    printf("\n Enter the age: ");
-    scanf("%d", &add.age);
-    printf("\n Enter your email address: ");
-    scanf("%s", add.email_addess);
-    printf("\n Enter id number: ");
-    scanf("%d", &add.id_no);
-    printf("\n Enter phone number: ");
-    scanf("%lf", &add.phone_no);
-    printf("\n Type of account: \n\t#Savings\n\t#Current\n\t#Fixed\n\nEnter your choice: ");
-    scanf("%s", add.acc_type);
-
-    fprintf(ptr, "\n%s %s %d %d %s %d %lf %s",
-            s, add.name, add.acc_no, add.age, add.email_addess, add.id_no, add.phone_no,
-            add.acc_type);
-
-    fclose(ptr);
-    printf("\nAccount created Successfully !!!");
-
-    printf("\nEnter 1 to go to main menu or 0 to exit: ");
-    scanf("%d", &main_exit);
-
-    if(main_exit == 1){
-        menu();
-    }else if(main_exit == 0){
-        close_app();
-    }
-    else{
-        printf("\nInvalid option");
-    }
 }
 
 int main(){
