513acaa2d386ef5be9639dd5fb47073a4197255b
ealpha072 <ealpha072@gmail.com>
2023-10-03 11:45:00 +0300

    Removed editor files

--- Text_Editor/kilo.c
+++ Text_Editor/kilo.c
@@ -2,15 +2,6 @@
 #include <unistd.h>;
 
 
-void enableRawMode(){
-	struct termios raw;
-	tcgetattr(STDIN_FILENO, $raw);
-	raw.c_lflag &= ~(ECHO);
-
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
-
 int main(){
 	enableRawMode();
 	//reading input from user
