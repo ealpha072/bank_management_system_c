a062054a768aff9809d6d353b10f9a07432eba73
ealpha072 <ealpha072@gmail.com>
2023-10-03 09:45:00 +0300

    Redone menu

--- bank_system.c
+++ bank_system.c
@@ -6,36 +6,36 @@
 int main_exit;
 
 void menu(void){
-    int choice;
+    int user_choice;
     system("cls");
     system("color 6");
     printf("\n\n\t\tCUSTOMER ACCOUNT BANK MANAGEMENT SYSTEM");
     printf("\n\n\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Welcome to the main menu \xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
     printf("\n\t1.Create new account");
-    printf("\n\t2.Update infomation of existing account");
-    printf("\n\t3.Transact");
-    printf("\n\t4.Check details of existing account");
-    printf("\n\t5.View customer list");
-    printf("\n\t6.Update infomation of existing account");
-    printf("\n\t7.Exit\n");
+    printf("\n\t2.Login to existing account");
+    printf("\n\t3.View customer list");
+    printf("\n\t4.Exit\n");
     printf("\n\n\tEnter your choice here: ");
 
-    scanf("%d", &choice);
-    printf("\n\tUser chosen option: %d\n", choice);
+    scanf("%d", &user_choice);
+    printf("\n\tUser chosen option: %d\n", user_choice);
     system("cls");
-    switch(choice){
-        case 7: close_app();
-            break;
-        default:printf("Invalid choice, please try again");
+
+    switch(user_choice){
+        case 4:
+            close_app();
             break;
     }
 }
 
-void close_app(){
-    printf("\n\nThank you for choosing Alpha's Bank system");
+int close_app(){
+    printf("\n\n\t\tThank you for choosing our bank\n\n");
 }
+
 
 int main(){
     menu();
     return 0;
 }
+
+
